d2ea6441203e5c3af9c941109c87436a99a58ac6
Joe Kirchner <joe.kirchner@gmail.com>
2025-02-09 20:05:01 -0500

    completed lessons through 3.21

--- phonebook/index.js
+++ phonebook/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'))
@@ -54,64 +55,88 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p>
-        <p>${Date()}</p>`)
+    Person.countDocuments({}).then(entries => {
+        response.send(`<p>Phonebook has info for ${entries} people</p>
+            <p>${Date()}</p>`)
+    })
+    
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(people => {
+        response.json(people)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+        if(person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
 const generateId = () => {
     return Math.round(Math.random() * 1000000)
 }
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
 
-    if(!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
+    // person.save().then(savedPerson => {
+    //     response.json(savedPerson)
+    // })
+    // .catch(error => next(error.message))
+   Person.findOne({ "name": body.name }, {runValidators: true})
+    .then(result => {
+        if (result != null) {
+            return response.status(400).json({
+                error: 'name already exists'
+            })
+        } else {
+            const person = new Person({
+                name: body.name,
+                number: body.number
+            })
+        
+            person.save().then(savedPerson => {
+                response.json(savedPerson)
+            })
+            .catch(error => next(error))            
+        }
+    })
 
-    if(!body.number) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
+})
 
-    if(persons.find(person => person.name === body.name)) {
-        return response.status(400).json({
-            error: 'name already exists'
-        })
-    }
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
 
     const person = {
         name: body.name,
         number: body.number,
-        id: generateId()
     }
-    persons = persons.concat(person)
 
-    response.json(person)
+    Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -119,6 +144,18 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'name must be three characters minimum' })
+    } else if (error.name === 'ValidationError'){
+        return response.status(400).json({ error: error.message })
+    } 
+
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
